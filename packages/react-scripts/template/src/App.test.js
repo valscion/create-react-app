9efbc7cbb97c9e6fd8c8be929e928185a1ceccd7
Vesa Laakso <laakso.vesa@gmail.com>
2016-11-17 15:57:34 +0200

    Showcase failing scenario for #1052
    The error I get on node v6.7.0 is visible in this gist:
    https://gist.github.com/valscion/25989e714380f2eabe52d170d6ca6f41

--- packages/react-scripts/template/src/App.test.js
+++ packages/react-scripts/template/src/App.test.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+  const fn = ({ a, ...otherProps }) => otherProps;
+
+  fn({ a: 1, b: 2 });
+
+  console.log(fn({ a: 1, b: 2 }));
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
